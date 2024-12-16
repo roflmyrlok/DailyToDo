d2a58758d738a2cd1e6426c8d53683ee53fef386
qualia4 <mpozdniakov@kse.org.ua>
2024-12-16 19:34:37 +0200

    task struct impelemtation

---
+++ DailyToDo/Model/Task.swift
@@ -0,0 +1,22 @@
+//
+//  Task.swift
+//  DailyToDo
+//
+//  Created by Максим Поздняков on 16.12.2024.
+//
+
+import Foundation
+
+struct Task: Codable {
+    var id: UUID
+    var name: String
+    var description: String
+    var done: Bool
+    
+    init(name: String, description: String, done: Bool = false) {
+        self.id = UUID()
+        self.name = name
+        self.description = description
+        self.done = done
+    }
+}
